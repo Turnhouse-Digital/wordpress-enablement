ec664e789e98ead743a81ade740cb65401b46fdd
Sam Fallowfield <sam.fallowfield1@gmail.com>
2024-03-09 09:51:01 +0000

    updated bucket.tf config

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -3,6 +3,8 @@ resource "aws_s3_bucket" "website_bucket" {
     #checkov:skip=CKV_AWS_21: we don't want versioning for now
     #checkov:skip=CKV_AWS_144: we don't want cross region replication
     #checkov:skip=CKV_AWS_18: we don't want logging
+    #checkov:skip=CKV2_AWS_61: life cycyle config
+    #checkov:skip=CKV2_AWS_62: We don't want to enable notifications
   bucket = "${locals.account_id}-website-bucket"
 }
 
@@ -20,12 +22,17 @@ resource "aws_s3_bucket" "website_bucket" {
  resource "aws_kms_key" "website_bucket_kms_key" {
     description = "This key is used to encrypt website-bucket objects"
     deletion_window_in_days = 7
+    enable_key_rotation = true
  }
 
  resource "aws_s3_bucket_public_access_block" "website_bucket_public_access" {
     bucket = aws_s3_bucket.website_bucket.bucket
-    block_public_acls = false
-    block_public_policy = false
+    # block_public_acls = false
+    # block_public_policy = false
+    block_public_acls = true
+    block_public_policy = true
+    restrict_public_buckets = true
+    ignore_public_acls = true
  }
 
  resource "aws_s3_bucket_versioning" "website_bucket_versioning" {
