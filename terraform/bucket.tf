c02aed6da29bee67558a40795bfa89686fccdd69
Sam Fallowfield <sam.fallowfield1@gmail.com>
2024-03-09 10:52:02 +0000

    fixed issue within the bucket.tf

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -5,7 +5,7 @@ resource "aws_s3_bucket" "website_bucket" {
   #checkov:skip=CKV_AWS_18: we don't want logging
   #checkov:skip=CKV2_AWS_61: life cycyle config
   #checkov:skip=CKV2_AWS_62: We don't want to enable notifications
-  bucket = "${locals.account_id}-website-bucket"
+  bucket = "${local.account_id}-website-bucket"
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "website_bucket_sse_config" {
@@ -23,6 +23,12 @@ resource "aws_kms_key" "website_bucket_kms_key" {
   description             = "This key is used to encrypt website-bucket objects"
   deletion_window_in_days = 7
   enable_key_rotation     = true
+  policy                  = aws_iam_policy.website_bucket_kms_key_policy.arn
+}
+
+resource "aws_iam_policy" "website_bucket_kms_key_policy" {
+  name        = "website_bucket_kms_key_policy"
+  description = "IAM policy to restrict KMS key usage to website bucket"
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
@@ -34,7 +40,7 @@ resource "aws_kms_key" "website_bucket_kms_key" {
         "kms:GenerateDataKey*",
         "kms:DescribeKey"
       ],
-      Resource = aws_kms_key.website_bucket_kms_key.arn,
+      #   Resource = "*",
       Condition = {
         StringEquals = {
           "kms:ViaService"                   = "s3.eu-west-2.amazonaws.com",
@@ -45,54 +51,29 @@ resource "aws_kms_key" "website_bucket_kms_key" {
   })
 }
 
-# resource "aws_iam_policy" "website_bucket_kms_key_policy" {
-#     name        = "website_bucket_kms_key_policy"
-#     description = "IAM policy to restrict KMS key usage to website bucket"
-#     policy      = jsonencode({
-#         Version = "2012-10-17",
-#         Statement = [{
-#         Effect   = "Allow",
-#         Action   = [
-#             "kms:Encrypt",
-#             "kms:Decrypt",
-#             "kms:ReEncrypt*",
-#             "kms:GenerateDataKey*",
-#             "kms:DescribeKey"
-#         ],
-#         Resource = aws_kms_key.website_bucket_kms_key.arn,
-#         Condition = {
-#             StringEquals = {
-#             "kms:ViaService" = "s3.eu-west-2.amazonaws.com",
-#             "kms:EncryptionContext:aws:s3:arn" = aws_s3_bucket.website_bucket.arn
-#             }
-#         }
-#         }]
-#     })
-# }
+resource "aws_iam_role" "website_bucket_role" {
+  name               = "website_bucket_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "s3.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
 
-# resource "aws_iam_role" "website_bucket_role" {
-#   name               = "website_bucket_role"
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Principal": {
-#         "Service": "s3.amazonaws.com"
-#       },
-#       "Action": "sts:AssumeRole"
-#     }
-#   ]
-# }
-# EOF
-# }
-
-# resource "aws_iam_policy_attachment" "kms_key_policy_attachment" {
-#   name       = "kms_key_policy_attachment_for_website_bucket"
-#   policy_arn = aws_iam_policy.website_bucket_kms_key_policy.arn
-#   roles      = [aws_iam_role.website_bucket_role.name]
-# }
+resource "aws_iam_policy_attachment" "kms_key_policy_attachment" {
+  name       = "kms_key_policy_attachment_for_website_bucket"
+  policy_arn = aws_iam_policy.website_bucket_kms_key_policy.arn
+  roles      = [aws_iam_role.website_bucket_role.name]
+}
 
 
 resource "aws_s3_bucket_public_access_block" "website_bucket_public_access" {
@@ -108,7 +89,7 @@ resource "aws_s3_bucket_public_access_block" "website_bucket_public_access" {
 resource "aws_s3_bucket_versioning" "website_bucket_versioning" {
   bucket = aws_s3_bucket.website_bucket.bucket
   versioning_configuration {
-    status = "disabled"
+    status = "Disabled"
   }
 }
 
