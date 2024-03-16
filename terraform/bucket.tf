5676b5798d7bce8f03b6d515182a948111d5140f
Sam Fallowfield <sam.fallowfield1@gmail.com>
2024-03-16 11:16:04 +0000

    updated Terraform WIP

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -5,6 +5,7 @@ resource "aws_s3_bucket" "website_bucket" {
   #checkov:skip=CKV_AWS_18: we don't want logging
   #checkov:skip=CKV2_AWS_61: life cycyle config
   #checkov:skip=CKV2_AWS_62: We don't want to enable notifications
+  #checkov:skip=CKV_AWS_70: We want everyone to be allowed to get the object 
   bucket = "${local.account_id}-website-bucket"
 }
 
@@ -17,13 +18,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "website_bucket_ss
       sse_algorithm     = "aws:kms"
     }
   }
-}
-
-resource "aws_kms_key" "website_bucket_kms_key" {
-  description             = "This key is used to encrypt website-bucket objects"
-  deletion_window_in_days = 7
-  enable_key_rotation     = true
-  policy                  = aws_iam_policy.website_bucket_kms_key_policy.arn
 }
 
 resource "aws_iam_policy" "website_bucket_kms_key_policy" {
@@ -40,7 +34,7 @@ resource "aws_iam_policy" "website_bucket_kms_key_policy" {
         "kms:GenerateDataKey*",
         "kms:DescribeKey"
       ],
-      #   Resource = "*",
+      Resource = aws_s3_bucket.website_bucket.arn,
       Condition = {
         StringEquals = {
           "kms:ViaService"                   = "s3.eu-west-2.amazonaws.com",
@@ -50,6 +44,15 @@ resource "aws_iam_policy" "website_bucket_kms_key_policy" {
     }]
   })
 }
+
+resource "aws_kms_key" "website_bucket_kms_key" {
+  description             = "This key is used to encrypt website-bucket objects"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+  policy                  = aws_iam_policy.website_bucket_kms_key_policy.policy
+}
+
+
 
 resource "aws_iam_role" "website_bucket_role" {
   name               = "website_bucket_role"
@@ -77,9 +80,9 @@ resource "aws_iam_policy_attachment" "kms_key_policy_attachment" {
 
 
 resource "aws_s3_bucket_public_access_block" "website_bucket_public_access" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  # block_public_acls = false
-  # block_public_policy = false
+  #checkov:skip=CKV_AWS_53: We want it to be publicly accessible
+  #checkov:skip=CKV_AWS_54: we want the block public policy set to false
+  bucket                  = aws_s3_bucket.website_bucket.id
   block_public_acls       = true
   block_public_policy     = true
   restrict_public_buckets = true
@@ -91,5 +94,23 @@ resource "aws_s3_bucket_versioning" "website_bucket_versioning" {
   versioning_configuration {
     status = "Disabled"
   }
+}
+
+resource "aws_s3_bucket_policy" "website_bucket_access_policy" {
+  #checkov:skip=CKV_AWS_70: We want everyone to be allowed to get the object 
+  bucket = aws_s3_bucket.website_bucket.id
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [{
+      "Sid" : "PublicReadGetObject",
+      "Effect" : "Allow",
+      "Principal" : "*",
+      "Action" : "s3:GetObject",
+    "Resource" : "${aws_s3_bucket.website_bucket.arn}/*" }]
+  })
+}
+
+output "website_url" {
+  value = aws_s3_bucket.website_bucket.website_endpoint
 }
 
