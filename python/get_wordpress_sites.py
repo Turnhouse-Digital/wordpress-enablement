f51077fdbb18b977876537502104b96a5329e554
Sam Fallowfield <sam.fallowfield1@gmail.com>
2024-04-06 12:31:24 +0100

    updated functionality to store results in an object as per error warning

--- python/get_wordpress_sites.py
+++ python/get_wordpress_sites.py
@@ -3,12 +3,11 @@ import json
 
 def list_website_directories():
     dir_path = "../bin/wordpress-sites/sites"
-    results = []
+    results = {}
 
     for path in os.listdir(dir_path):
         if os.path.isdir(os.path.join(dir_path, path)):
-            results.append(path)
+            results[path] = path  # Use directory name as value
     print(json.dumps(results))
 
 list_website_directories()
-
