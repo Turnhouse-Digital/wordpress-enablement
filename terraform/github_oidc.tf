b68e42004d16182924873db43f678796efbe6d71
Tom Vaughan <tomvaughan77@gmail.com>
2024-05-14 10:14:04 +0100

    Added wordpress-sites repo to allow it to deploy sites, and also updated OIDC terraform lib

--- terraform/github_oidc.tf
+++ terraform/github_oidc.tf
@@ -1,9 +1,10 @@
 #checkov:skip=CKV_AWS_274: The role needs admin access to deploy/destroy anything
 module "oidc_github" {
-  source = "git::https://github.com/unfunco/terraform-aws-oidc-github.git?ref=6aed749fc1cdbff25a0052eec5ae9a2d584507e9" # v1.7.1
+  source = "git::https://github.com/unfunco/terraform-aws-oidc-github.git?ref=80358ac71bcbb49dd5807486682a2b1d81cdf15c" # v1.8.0
 
   github_repositories = [
     "tomvaughan77/wordpress-enablement",
+    "tomvaughan77/wordpress-sites",
   ]
   iam_role_name       = "GithubActionsTerraformDeployRole"
   attach_admin_policy = true
